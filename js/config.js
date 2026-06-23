ebd88e1ab346dc8aa487f5f9a4a8e397a2626ff8
alinaresblanco-gif <alinares.blanco@gmail.com>
2026-06-23 18:24:10 +0200

    feat: desplegar API gratis en Supabase y conectar frontends

--- js/config.js
+++ js/config.js
@@ -3,5 +3,5 @@
 (function initLinclockConfig() {
 	// URL base de la API. En GitHub Pages debe apuntar a Supabase Functions.
 	// En local, si no se define, se usa window.location.origin desde app.js.
-	window.LINCLOCK_API_BASE = '';
+	window.LINCLOCK_API_BASE = 'https://qtyecwfhbxwunmjdtkoa.supabase.co/functions/v1/linclock-api';
 })();
